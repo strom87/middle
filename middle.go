fe97d370ed7a33205baac423a35ea1a258ba57eb
strom87 <daniel.strom.87@gmail.com>
2016-03-27 01:49:13 +0100

    fixed dirty code

--- middle.go
+++ middle.go
@@ -8,17 +8,14 @@ type wrappedRequest func(wr http.ResponseWriter, r *http.Request, next Request)
 type middleware func(wr http.ResponseWriter, r *http.Request) bool
 
 type wrapper struct {
-	before     []middleware
-	after      []middleware
-	wrapped    wrappedRequest
-	useBefore  []middleware
-	useAfter   []middleware
-	useWrapped wrappedRequest
+	before  []middleware
+	after   []middleware
+	wrapped wrappedRequest
 }
 
 // New returns a instance of the wrapper
 func New() *wrapper {
-	return &wrapper{wrapped: nil, useWrapped: nil}
+	return &wrapper{wrapped: nil}
 }
 
 // Before is used when chaining middleware for calls before the main request
@@ -41,39 +38,32 @@ func (w wrapper) Wrap(wrapped wrappedRequest) wrapper {
 
 // UseBefore handles all the middleware calls before the main request for all the routes
 func (w *wrapper) UseBefore(middlewares ...middleware) {
-	w.appendMiddlewares(&w.useBefore, middlewares)
+	w.appendMiddlewares(&w.before, middlewares)
 }
 
 // UseAfter handles all the middleware calls after the main request for all the routes
 func (w *wrapper) UseAfter(middlewares ...middleware) {
-	w.appendMiddlewares(&w.useAfter, middlewares)
+	w.appendMiddlewares(&w.after, middlewares)
 }
 
 // UseWrap encapsulates the main request for all the routes
 func (w *wrapper) UseWrap(wrapped wrappedRequest) {
-	w.useWrapped = wrapped
+	w.wrapped = wrapped
 }
 
 // Then runs the request and executes all the middlewares
 func (w wrapper) Then(req Request) Request {
 	return func(wr http.ResponseWriter, r *http.Request) {
-		if ok := w.executeMiddlewares(w.useBefore, wr, r); !ok {
-			return
-		} else if ok := w.executeMiddlewares(w.before, wr, r); !ok {
+		if ok := w.executeMiddlewares(w.before, wr, r); !ok {
 			return
 		}
 
 		if w.wrapped != nil {
 			w.wrapped(wr, r, req)
-		} else if w.useWrapped != nil {
-			w.useWrapped(wr, r, req)
 		} else {
 			req(wr, r)
 		}
 
-		if ok := w.executeMiddlewares(w.useAfter, wr, r); !ok {
-			return
-		}
 		w.executeMiddlewares(w.after, wr, r)
 	}
 }
